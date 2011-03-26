62a3644ceaf1d9732d57721e30b24485e84ae755
Seth Falcon <seth@userprimary.net>
2011-03-26 15:34:29 -0700

    Add some specs and refactor error case returns

--- src/chef_otto.erl
+++ src/chef_otto.erl
@@ -10,6 +10,10 @@
          start0/0
          ]).
 
+-type couchbeam_server() :: any().
+-type http_port() :: non_neg_integer().
+-type user() :: binary() | string().
+
 -define(gv(Key, PList), proplists:get_value(Key, PList)).
 
 -define(user_db, "opscode_account").
@@ -28,12 +32,19 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+-spec connect() -> couchbeam_server().
 connect() ->
     connect("localhost", 5984).
 
+-spec connect(string(), http_port()) -> couchbeam_server().
 connect(Host, Port) ->
     couchbeam:server_connection(Host, Port, "", []).
 
+-spec fetch_user(couchbeam_server(), user()) -> [tuple()]
+                                                    | {user_not_found,
+                                                       not_in_view}
+                                                    | {user_not_found,
+                                                       {no_doc, binary()}}.
 fetch_user(Server, User) when is_binary(User) ->
     {ok, Db} = couchbeam:open_db(Server, ?user_db, []),
     {ok, View} = couchbeam:view(Db, {?mixlib_auth_user_design, "by_username"},
@@ -51,6 +62,10 @@ fetch_user(Server, User) when is_binary(User) ->
 fetch_user(Server, User) when is_list(User) ->
     fetch_user(Server, list_to_binary(User)).
 
+-spec fetch_org(couchbeam_server(), binary()) ->
+    [tuple()]
+        | {org_not_found, not_in_view}
+        | {org_not_found, {no_doc, binary()}}.
 fetch_org(Server, OrgName) when is_binary(OrgName) ->
     {ok, Db} = couchbeam:open_db(Server, ?user_db, []),
     {ok, View} = couchbeam:view(Db, {?mixlib_auth_org_design, "by_name"},
@@ -59,16 +74,22 @@ fetch_org(Server, OrgName) when is_binary(OrgName) ->
         {ok, {Row}} ->
             OrgDocId = ?gv(<<"id">>, Row),
             case couchbeam:open_doc(Db, OrgDocId) of
-                {error, not_found} -> {org_not_found, OrgName, {no_doc, OrgDocId}};
+                {error, not_found} -> {org_not_found, {no_doc, OrgDocId}};
                 {ok, {OrgDoc}} -> OrgDoc
             end;
         {ok, []} ->
-            {org_not_found, OrgName, not_in_view}
+            {org_not_found, not_in_view}
     end;
 fetch_org(Server, OrgName) when is_list(OrgName) ->
     fetch_org(Server, list_to_binary(OrgName)).
 
-fetch_client(Server, Org, ClientName) when is_binary(ClientName); is_list(Org) ->
+-spec fetch_client(couchbeam_server(), [tuple()] | {org_not_found, _},
+                   binary() | string()) ->
+    [tuple()]
+        | {client_not_found, not_in_view}
+        | {client_not_found, {no_doc, binary()}}
+        | {client_not_found, {org_not_found, _}}.
+fetch_client(Server, Org, ClientName) when is_binary(ClientName), is_list(Org) ->
     ChefDb = "chef_" ++ ?gv(<<"guid">>, Org),
     {ok, Db} = couchbeam:open_db(Server, ChefDb, []),
     {ok, View} = couchbeam:view(Db, {?mixlib_auth_client_design, "by_clientname"},
@@ -77,17 +98,19 @@ fetch_client(Server, Org, ClientName) when is_binary(ClientName); is_list(Org) -
         {ok, {Row}} ->
             ClientId = ?gv(<<"id">>, Row),
             case couchbeam:open_doc(Db, ClientId) of
-                {error, not_found} -> {client_not_found, ClientName, {no_doc, ClientId}};
+                {error, not_found} -> {client_not_found, {no_doc, ClientId}};
                 {ok, {ClientDoc}} -> ClientDoc
             end;
         {ok, []} ->
-            {client_not_found, ClientName, not_in_view}
+            {client_not_found, not_in_view}
     end;
-fetch_client(Server, Org, ClientName) when is_list(ClientName); is_list(Org) ->
+fetch_client(Server, Org, ClientName) when is_list(ClientName), is_list(Org) ->
     fetch_client(Server, Org, list_to_binary(ClientName));
-fetch_client(Server, Reason={org_not_found, OrgName, Why}, ClientName) ->
-    {client_not_found, ClientName, Reason}.
+fetch_client(_Server, Reason={org_not_found, _}, _ClientName) ->
+    {client_not_found, Reason}.
 
+-spec bulk_get(couchbeam_server(), string(), [binary()]) ->
+    [[tuple()]].
 bulk_get(Server, DbName, Ids) ->
     {ok, Db} = couchbeam:open_db(Server, DbName, []),
     {ok, View} = couchbeam:all_docs(Db, [{keys, Ids}, {include_docs, true}]),
@@ -130,13 +153,42 @@ otto_integration_test_() ->
       end
      },
 
+
+     {"fetch_org not found",
+      fun() ->
+              ?assertEqual({org_not_found, not_in_view},
+                           chef_otto:fetch_org(S, <<"no-such-org">>))
+              % FIXME: how can we test the case when org is in view,
+              % but not found in the db.  Need to either manipulate a
+              % test couchdb or introduce some mocks.
+      end
+     },
+
      {"fetch_client",
       fun() ->
               Org = chef_otto:fetch_org(S, <<"clownco">>),
               Client = chef_otto:fetch_client(S, Org, <<"clownco-validator">>),
               ?assertEqual(<<"clownco">>, ?gv(<<"orgname">>, Client))
       end
+     },
+
+     {"fetch_client no such client",
+      fun() ->
+              Org = chef_otto:fetch_org(S, <<"clownco">>),
+              ?assertEqual({client_not_found, not_in_view},
+                 chef_otto:fetch_client(S, Org, <<"not-a-known-client">>))
+      end
+     },
+
+     {"fetch_client with missing org",
+      fun() ->
+              Org = chef_otto:fetch_org(S, <<"no-such-org">>),
+              ?assertEqual({client_not_found, {org_not_found, not_in_view}},
+                 chef_otto:fetch_client(S, Org, <<"not-a-known-client">>))
+      end
      }
+
+
 
      ].
     
